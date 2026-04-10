bd37c892555de7e52b21e8af67ef08dff7033e95
Crazelu <eberelucky64@gmail.com>
2026-04-10 12:25:12 +0100

    test: adds more cron parser tests

--- packages/serverpod/test/server/cron_test.dart
+++ packages/serverpod/test/server/cron_test.dart
@@ -3,6 +3,116 @@ import 'package:test/test.dart';
 
 void main() {
   test(
+    'Given a cron expression with less than 5 fields, '
+    'then a CronFormatException is thrown',
+    () {
+      expect(
+        () => Cron.parse('* * *'),
+        throwsA(
+          isA<CronFormatException>().having(
+            (e) => e.message,
+            'message',
+            'Invalid cron expression: * * *. Only 5-field and 6-field formats are supported.',
+          ),
+        ),
+      );
+
+      expect(
+        () => Cron.parse('* * * *'),
+        throwsA(
+          isA<CronFormatException>().having(
+            (e) => e.message,
+            'message',
+            'Invalid cron expression: * * * *. Only 5-field and 6-field formats are supported.',
+          ),
+        ),
+      );
+    },
+  );
+
+  test(
+    'Given a cron expression with more than 6 fields, '
+    'when parsing, '
+    'then a CronFormatException is thrown',
+    () {
+      expect(
+        () => Cron.parse('* * * * * * *'),
+        throwsA(
+          isA<CronFormatException>().having(
+            (e) => e.message,
+            'message',
+            'Invalid cron expression: * * * * * * *. Only 5-field and 6-field formats are supported.',
+          ),
+        ),
+      );
+      expect(
+        () => Cron.parse('* * * * * * * *'),
+        throwsA(
+          isA<CronFormatException>().having(
+            (e) => e.message,
+            'message',
+            'Invalid cron expression: * * * * * * * *. Only 5-field and 6-field formats are supported.',
+          ),
+        ),
+      );
+    },
+  );
+
+  test(
+    'Given a cron expression with an invalid interval, '
+    'when parsing, '
+    'then a CronFormatException is thrown',
+    () {
+      expect(
+        () => Cron.parse('*/* * * * *'),
+        throwsA(
+          isA<CronFormatException>().having(
+            (e) => e.message,
+            'message',
+            'Invalid cron expression. Invalid interval value: *',
+          ),
+        ),
+      );
+    },
+  );
+
+  test(
+    'Given a cron expression with an interval value less than 1, '
+    'when parsing, '
+    'then a CronFormatException is thrown',
+    () {
+      expect(
+        () => Cron.parse('*/0 * * * *'),
+        throwsA(
+          isA<CronFormatException>().having(
+            (e) => e.message,
+            'message',
+            'Invalid cron expression. Invalid interval value: 0',
+          ),
+        ),
+      );
+    },
+  );
+
+  test(
+    'Given a cron expression with an interval containing multiple `/`, '
+    'when parsing, '
+    'then a CronFormatException is thrown',
+    () {
+      expect(
+        () => Cron.parse('*/2/3 * * * *'),
+        throwsA(
+          isA<CronFormatException>().having(
+            (e) => e.message,
+            'message',
+            'Invalid cron expression. More than one `/` for intervals: */2/3',
+          ),
+        ),
+      );
+    },
+  );
+
+  test(
     'Given an invalid cron expression, '
     'when parsing, '
     'then a CronFormatException is thrown',
@@ -11,22 +121,87 @@ void main() {
         () => Cron.parse('invalid'),
         throwsA(isA<CronFormatException>()),
       );
-
-      expect(
-        () => Cron.parse('* * *'),
-        throwsA(isA<CronFormatException>()),
-      );
     },
   );
 
   test(
-    'Given a valid 5-field cron expression, '
+    'Given a valid 5-field cron expression with single value fields, '
     'when parsing, '
     'then all fields are parsed correctly',
     () {
-      final schedule = Cron.parse('1 13 */2 3-6 *');
+      final schedule = Cron.parse('1 13 3 3 6');
       expect(schedule.seconds, isNull);
       expect(schedule.minutes, [1]);
+      expect(schedule.hours, [13]);
+      expect(schedule.days, [3]);
+      expect(schedule.months, [3]);
+      expect(schedule.weekdays, [6]);
+    },
+  );
+
+  test(
+    'Given a valid 5-field cron expression with wildcard fields, '
+    'when parsing, '
+    'then all fields are parsed correctly',
+    () {
+      final schedule = Cron.parse('* * * * *');
+      expect(schedule.seconds, isNull);
+      expect(schedule.minutes, List.generate(60, (i) => i));
+      expect(schedule.hours, List.generate(24, (i) => i));
+      expect(schedule.days, List.generate(31, (i) => i + 1));
+      expect(schedule.months, List.generate(12, (i) => i + 1));
+      expect(schedule.weekdays, [7, 1, 2, 3, 4, 5, 6]);
+    },
+  );
+
+  test(
+    'Given a valid 5-field cron expression with a field containing comma-separated values, '
+    'when parsing, '
+    'then the field is parsed correctly',
+    () {
+      final schedule = Cron.parse('0,15,30,45 * * * *');
+      expect(schedule.minutes, [0, 15, 30, 45]);
+    },
+  );
+
+  test(
+    'Given a valid 5-field cron expression with a field containing an n-th interval, '
+    'when parsing, '
+    'then the field is parsed correctly',
+    () {
+      final schedule = Cron.parse('*/15 * * * *');
+      expect(schedule.minutes, [0, 15, 30, 45]);
+    },
+  );
+
+  test(
+    'Given a valid 5-field cron expression with a field containing a range, '
+    'when parsing, '
+    'then the field is parsed correctly',
+    () {
+      final schedule = Cron.parse('1-5 * * * *');
+      expect(schedule.minutes, [1, 2, 3, 4, 5]);
+    },
+  );
+
+  test(
+    'Given a valid 5-field cron expression with a field containing a range interval with increment, '
+    'when parsing, '
+    'then the field is parsed correctly',
+    () {
+      final schedule = Cron.parse('0-10/2 * * * *');
+      expect(schedule.minutes, [0, 2, 4, 6, 8]);
+    },
+  );
+
+  test(
+    'Given a valid 5-field cron expression with mixed field types, '
+    'when parsing, '
+    'then all fields are parsed correctly',
+    () {
+      final schedule = Cron.parse('0-8/4 13 */2 3-6 1,2');
+      expect(schedule.seconds, isNull);
+      expect(schedule.minutes, [0, 4]);
       expect(schedule.hours, [13]);
       expect(schedule.days, [
         2,
@@ -46,19 +221,34 @@ void main() {
         30,
       ]);
       expect(schedule.months, [3, 4, 5, 6]);
-      expect(schedule.weekdays, [7, 1, 2, 3, 4, 5, 6]);
+      expect(schedule.weekdays, [1, 2]);
     },
   );
 
   test(
-    'Given a valid 5-field cron expression, '
+    'Given a valid 6-field cron expression with single value fields, '
     'when parsing, '
     'then all fields are parsed correctly',
     () {
-      final schedule = Cron.parse('0,1 0 * * * ');
-      expect(schedule.seconds, isNull);
-      expect(schedule.minutes, [0, 1]);
-      expect(schedule.hours, [0]);
+      final schedule = Cron.parse('30 1 13 3 3 6');
+      expect(schedule.seconds, [30]);
+      expect(schedule.minutes, [1]);
+      expect(schedule.hours, [13]);
+      expect(schedule.days, [3]);
+      expect(schedule.months, [3]);
+      expect(schedule.weekdays, [6]);
+    },
+  );
+
+  test(
+    'Given a valid 6-field cron expression with wildcard fields, '
+    'when parsing, '
+    'then all fields are parsed correctly',
+    () {
+      final schedule = Cron.parse('* * * * * *');
+      expect(schedule.seconds, List.generate(60, (i) => i));
+      expect(schedule.minutes, List.generate(60, (i) => i));
+      expect(schedule.hours, List.generate(24, (i) => i));
       expect(schedule.days, List.generate(31, (i) => i + 1));
       expect(schedule.months, List.generate(12, (i) => i + 1));
       expect(schedule.weekdays, [7, 1, 2, 3, 4, 5, 6]);
@@ -66,29 +256,64 @@ void main() {
   );
 
   test(
-    'Given a valid 6-field cron expression, '
+    'Given a valid 6-field cron expression with a field containing comma-separated values, '
+    'when parsing, '
+    'then the field is parsed correctly',
+    () {
+      final schedule = Cron.parse('0,15,30,45 * * * * *');
+      expect(schedule.seconds, [0, 15, 30, 45]);
+    },
+  );
+
+  test(
+    'Given a valid 6-field cron expression with a field containing an n-th interval, '
+    'when parsing, '
+    'then the field is parsed correctly',
+    () {
+      final schedule = Cron.parse('*/15 * * * * *');
+      expect(schedule.seconds, [0, 15, 30, 45]);
+    },
+  );
+
+  test(
+    'Given a valid 6-field cron expression with a field containing a range, '
+    'when parsing, '
+    'then the field is parsed correctly',
+    () {
+      final schedule = Cron.parse('1-5 * * * * *');
+      expect(schedule.seconds, [1, 2, 3, 4, 5]);
+    },
+  );
+
+  test(
+    'Given a valid 6-field cron expression with a field containing a range interval with increment, '
+    'when parsing, '
+    'then the field is parsed correctly',
+    () {
+      final schedule = Cron.parse('0-10/2 * * * * *');
+      expect(schedule.seconds, [0, 2, 4, 6, 8]);
+    },
+  );
+
+  test(
+    'Given a valid 6-field cron expression with mixed field types, '
     'when parsing, '
     'then all fields are parsed correctly',
     () {
-      final schedule = Cron.parse('1,30-31 1 13 */2 3-6 2,4');
+      final schedule = Cron.parse('1,30-31 1 13 */3 3-6 2,4');
       expect(schedule.seconds, [1, 30, 31]);
       expect(schedule.minutes, [1]);
       expect(schedule.hours, [13]);
       expect(schedule.days, [
-        2,
-        4,
+        3,
         6,
-        8,
-        10,
+        9,
         12,
-        14,
-        16,
+        15,
         18,
-        20,
-        22,
+        21,
         24,
-        26,
-        28,
+        27,
         30,
       ]);
       expect(schedule.months, [3, 4, 5, 6]);
